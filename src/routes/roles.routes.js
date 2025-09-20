3747857d1868677f57747b9869b4dc24eb45f3bf
Cristina-L25 <crislopez.bam@gmail.com>
2025-09-20 07:52:47 -0500

    Correción de errores y backend funcional

--- src/routes/roles.routes.js
+++ src/routes/roles.routes.js
@@ -4,10 +4,10 @@ const RolesController = require('../controllers/roles.controller');
 const router = express.Router();
 const rolesController = new RolesController();
 
-router.get('/', rolesController.obtenerRoles(req, res));
-router.get('/:id', rolesController.obtenerRolPorId(req, res));
-router.post('/', rolesController.agregarRol(req, res));
-router.put('/:id', rolesController.actualizarRol(req, res));
-router.delete('/:id', rolesController.eliminarRol(req, res));
+router.get('/', (req, res) => rolesController.obtenerRoles(req, res));
+router.get('/:id', (req, res) => rolesController.obtenerRolPorId(req, res));
+router.post('/', (req, res) => rolesController.agregarRol(req, res));
+router.put('/:id', (req, res) => rolesController.actualizarRol(req, res));
+router.delete('/:id', (req, res) => rolesController.eliminarRol(req, res));
 
 module.exports = router;
--- src/routes/roles_permisos.routes.js
+++ src/routes/roles_permisos.routes.js
@@ -4,9 +4,9 @@ const RolesPermisosController = require('../controllers/roles_permisos.controlle
 const router = express.Router();
 const rolesPermisosController = new RolesPermisosController();
 
-router.get('/', (req, res) => rolesPermisosController.obtenerRolesPermisos(req, res));
-router.get('/rol/:idrol', (req, res) => rolesPermisosController.obtenerRolPermisosDeRol(req, res));
-router.get('/:id', (req, res) => rolesPermisosController.obtenerRolPermisosDePermiso(req, res));
+router.get('/', (req, res) => rolesPermisosController.obtenerRolPermisos(req, res));
+router.get('/rol/:idrol', (req, res) => rolesPermisosController.obtenerPermisosDeRol(req, res));
+router.get('/:id', (req, res) => rolesPermisosController.obtenerRolPermisoPorId(req, res));
 router.post('/', (req, res) => rolesPermisosController.agregarRolPermiso(req, res));
 router.put('/:id', (req, res) => rolesPermisosController.actualizarRolPermiso(req, res));
 router.delete('/:id', (req, res) => rolesPermisosController.eliminarRolPermiso(req, res));
